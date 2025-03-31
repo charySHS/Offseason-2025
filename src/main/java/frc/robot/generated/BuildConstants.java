9da7a86bf8ac2a84d4f7e8a1bff9e1844a9aaa57
charyshs <smkzachatac@gmail.com>
2025-03-31 15:29:53 -0700

    First commit of offseason 2025 code. TODO: Change robot structure for singletons and change subsystem design

---
+++ src/main/java/frc/robot/generated/BuildConstants.java
@@ -0,0 +1,19 @@
+package frc.robot.generated;
+
+/**
+ * Automatically generated file containing build version information.
+ */
+public final class BuildConstants {
+  public static final String MAVEN_GROUP = "";
+  public static final String MAVEN_NAME = "Offseason-2025";
+  public static final String VERSION = "unspecified";
+  public static final int GIT_REVISION = -1;
+  public static final String GIT_SHA = "UNKNOWN";
+  public static final String GIT_DATE = "UNKNOWN";
+  public static final String GIT_BRANCH = "UNKNOWN";
+  public static final String BUILD_DATE = "2025-03-31 15:28:18 PDT";
+  public static final long BUILD_UNIX_TIME = 1743460098565L;
+  public static final int DIRTY = 129;
+
+  private BuildConstants(){}
+}
